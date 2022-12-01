45f2be70c4afd6f076b45c7f52f1a4c86fd42618
amrutha-b-nair <amruthabn00@gmail.com>
2022-12-01 15:36:22 +0100

    Completed

--- Day 1/day1.py
+++ Day 1/day1.py
@@ -3,19 +3,19 @@ with open('input.txt') as file:
     calories = file.read().split('\n')
 
    
-total_calories = []
-tot_calorie = 0
+total_calories_elf = []
+total_calorie = 0
 # most_calories = 0
 
 for calorie in calories:
     if calorie != "":
-        tot_calorie += int(calorie)
+        total_calorie += int(calorie)
     else:
-        total_calories.append(tot_calorie)
+        total_calories_elf.append(total_calorie)
         # most_calories = max(most_calories, tot_calorie)
-        tot_calorie = 0
+        total_calorie = 0
 
-total_calories.sort(reverse=True)
+total_calories_elf.sort(reverse=True)
 
-print("Part 1:", total_calories[0])
-print("Part 2:", sum(total_calories[0:3]))
+print("Part 1:", total_calories_elf[0])
+print("Part 2:", sum(total_calories_elf[0:3]))
